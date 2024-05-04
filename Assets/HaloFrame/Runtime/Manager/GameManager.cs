74b7bcf05c410278d6720fd08c03d5148c712a32
Halo <gsm958708323@gmail.com>
2024-05-04 16:02:25 +0800

    代码位置调整

--- Assets/HaloFrame/Runtime/Manager/GameManager.cs
+++ Assets/HaloFrame/Runtime/Manager/GameManager.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using HaloFrame;
 
-    public class GameManager : GameManagerBase
+public class GameManager : GameManagerBase
+{
+    public static DriverManager DriverManager;
+    public static RedDotManager RedDotManager;
+
+    protected override void InitManager()
     {
-        public static DriverManager DriverManager;
-        public static RedDotManager RedDotManager;
+        base.InitManager();
 
-        protected override void InitManager()
-        {
-            base.InitManager();
-
-            DriverManager = GetManager<DriverManager>();
-            RedDotManager = GetManager<RedDotManager>();
-        }
+        DriverManager = GetManager<DriverManager>();
+        RedDotManager = GetManager<RedDotManager>();
     }
+
+    /*
+    GameManager.RedDotManager.AddValue(redDotItem.Key, 1); 
+    上面的调用方式会走Awake方法，然后将GameManager的实例字段instance赋值，但是需要将GameManager挂载在GameObject上
+    */
+}
