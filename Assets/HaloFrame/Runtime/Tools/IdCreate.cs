dc80d8c1750885cb8d81575abe3b71504a3eed89
Halo <gsm958708323@gmail.com>
2026-01-02 08:50:49 +0800

    文件路径修改

---
+++ Assets/HaloFrame/Runtime/Tools/IdCreate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class IdCreate
+{
+    public int count = 0;
+
+    public int Get()
+    {
+        return ++count;
+    }
+}
